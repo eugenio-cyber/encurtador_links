b1f853dc57af32f65a8907878e416eb6344deae6
Wesley Costa <wesleyeugenio3@gmail.com>
2025-01-03 10:22:01 -0300

    fix: remove env variable requirement

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/home";
 import Links from "./pages/links";
 import Error from "./pages/error";
 
 export default function RoutesApp() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename='/'>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/links" element={<Links />}></Route>
-        <Route path="*" element={<Error />} />
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/links' element={<Links />}></Route>
+        <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
